17c476d1e0359b3ce88ab737e78c54da537f189a
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2023-11-21 18:58:37 -0300

    Fix user role assigning

--- src/Listeners/Auth/LoginListener.php
+++ src/Listeners/Auth/LoginListener.php
@@ -4,7 +4,6 @@ namespace IvaoBrasil\Infrastructure\Listeners\Auth;
 
 use Illuminate\Auth\Events\Login;
 use InvalidArgumentException;
-use IvaoBrasil\Infrastructure\Contracts\Auth\UserRolesInterface;
 use IvaoBrasil\Infrastructure\Services\Auth\RoleRegistrarService;
 
 class LoginListener
@@ -25,14 +24,17 @@ class LoginListener
     public function handle(Login $event): void
     {
         if (
-            !method_exists($event->user, 'assignRoles') ||
-            !property_exists($event->user, 'division') ||
-            !property_exists($event->user, 'staff')
+            !array_key_exists('division', $event->user->getAttributes()) ||
+            !array_key_exists('staff', $event->user->getAttributes())
         ) {
             return;
         }
 
-        $event->user->assignRoles(
+        if (!in_array(\Spatie\Permission\Traits\HasRoles::class, class_uses($event->user))) {
+            return;
+        }
+
+        $event->user->syncRoles(
             $this->roleRegistrar->getRolesToAssign(
                 $event->user->division,
                 $event->user->staff
